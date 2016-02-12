bbc012771a7bf002a239c5992c7430a0bb8b4f76
Alex Legg <alexander.legg@nicta.com.au>
2016-02-12 17:42:14 +1100

    User get/create API and tests

--- src/CouchGames/Server.hs
+++ src/CouchGames/Server.hs
@@ -1,37 +1,87 @@
-{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
-module CouchGames.Server (runApp, app) where
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, DataKinds, TypeFamilies #-}
+module CouchGames.Server (runApp, appTest) where
 
 import           Data.Aeson (Value(..), object, (.=))
-import           Network.Wai (Application)
+import           Data.Int
+import           Data.Maybe
+import qualified Data.Text as T
+import           Network.Wai (Application, Middleware)
+import           Network.HTTP.Types.Status
 import           Web.Users.Types
 import           Web.Users.Postgresql ()
 import           Web.Spock.Safe
+import           Control.Monad.IO.Class
 import           Database.PostgreSQL.Simple
 import qualified CouchGames.Config as C
 
-app' userBackend = spockT id $ do
+app' :: Connection -> IO Middleware
+app' conn = spockT id $ do
     get "/" $ do
         text "hello"
 
     get "/some-json" $ do
         json $ object ["foo" .= Number 23, "bar" .= Number 42]
 
----    S.post "user/register" $ do
----        json 
+    get getUser $ \userId -> do
+        u <- liftIO $ (getUserById conn userId :: IO (Maybe CouchUser))
+        case u of
+            (Just user) -> json $ object ["userId" .= userId, "name" .= u_name user]
+            Nothing     -> sendError 1 "User not found"
 
-app :: IO Application
-app = do
+    post newUser $ do
+        userName        <- param "name"
+        userEmail       <- param "email"
+        userPassword    <- param "password"
+        case (userName, userEmail, userPassword) of
+            (Just n, Just e, Just p)    -> do
+                u <- liftIO $ createUser conn (mkCouchUser n e p)
+                case u of
+                    Left InvalidPassword        -> sendError 3 "Invalid password"
+                    Left UsernameAlreadyTaken   -> sendError 4 "Username already taken"
+                    Left EmailAlreadyTaken      -> sendError 5 "Email already taken"
+                    Right uid                   -> json $ object ["userId" .= uid, "name" .= n]
+                    _                           -> sendError 6 "Unspecified error"
+            _                                   -> sendError 2 "Missing parameters"
+
+sendError errorCode msg = do
+    setStatus notFound404
+    json $ object ["code" .= Number errorCode, "message" .= String msg]
+
+{- User API -}
+
+type CouchUser = User ()
+
+mkCouchUser :: T.Text -> T.Text -> T.Text -> CouchUser
+mkCouchUser name email password = User
+    { u_name        = name
+    , u_email       = email
+    , u_password    = makePassword (PasswordPlain password)
+    , u_active      = True
+    , u_more        = ()
+    }
+
+getUser :: Path '[Int64]
+getUser = "users" <//> var
+
+newUser :: Path '[]
+newUser = "users" <//> "register" 
+
+{- End User API -}
+
+appTest :: IO Application
+appTest = do
     config      <- C.parseConfig "config.yaml"
     conn        <- connectToDatabase config
-    userBackend <- initUserBackend conn
-    spockAsApp (app' userBackend)
+    destroyUserBackend conn
+    initUserBackend conn
+    spockAsApp (app' conn)
 
 runApp :: IO ()
 runApp = do
     config      <- C.parseConfig "config.yaml"
     conn        <- connectToDatabase config
-    userBackend <- initUserBackend conn
-    runSpock 80 (app' userBackend)
+    initUserBackend conn
+    runSpock 80 (app' conn)
 
 connectToDatabase (Just config) = do
     connect defaultConnectInfo
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,17 +5,22 @@ import           Test.Hspec
 import           Test.Hspec.Wai
 import           Test.Hspec.Wai.JSON
 import           Control.Exception (bracket)
-import           Data.Aeson (Value(..), object, (.=))
+import           Data.Aeson (Value(..), object, (.=), (.:), Object(..), decode, FromJSON(..))
+import           Network.Wai.Test (SResponse(..))
+import           Network.HTTP.Types.Status
 
-import           CouchGames.Server (app)
+import           CouchGames.Server (appTest)
 import           CouchGames.Player
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Map as M
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-    with app $ do
+    with appTest $ do
         describe "GET /" $ do
             it "responds with 200" $ do
                 get "/" `shouldRespondWith` 200
@@ -33,7 +38,40 @@ spec = do
             it "responds with some JSON" $ do
                 get "/some-json" `shouldRespondWith` [json|{foo: 23, bar: 42}|]
 
+        describe "GET /users" $ do
+            it "fails to get an invalid id" $ do
+                get "/users/1" `shouldRespondWith` [json|{code: 1, message:"User not found"}|] {matchStatus = 404}
 
----    describe "Lobby" $ do
----        it "creates a lobby" $ do
----            createLobby 
+            it "creates a user" $ do
+                (SResponse s _ b) <- postHtmlForm "/users/register" [("name", "test_username"), ("email", "test@test.com"), ("password", "pass1234")]
+                liftIO $ statusCode s `shouldBe` 200
+                case (decode b :: Maybe RegisterUserR) of
+                    Just rur    -> do
+                        (SResponse s _ b) <- get (BS.append "/users/" (BS.pack (show (rurUserId rur))))
+                        liftIO $ statusCode s `shouldBe` 200
+                        case (decode b :: Maybe GetUserR) of 
+                            Just gur    -> liftIO $ gurUserId gur `shouldBe` rurUserId rur
+                            Nothing     -> liftIO $ expectationFailure "Could not decode response"
+                    Nothing     -> liftIO $ expectationFailure "Could not decode response"
+
+            it "fails to create a duplicate user" $ do
+                postHtmlForm "/users/register" [("name", "test_username"), ("email", "test2@test.com"), ("password", "1223pass")]
+                    `shouldRespondWith` [json|{code: 4, message: "Username already taken"}|] {matchStatus = 404}
+
+data GetUserR = GetUserR
+    { gurUserId :: Int
+    , gurName   :: String
+    } deriving (Show, Eq)
+
+instance FromJSON GetUserR where
+    parseJSON (Object v) =
+        GetUserR <$> (v .: "userId") <*> (v .: "name")
+
+data RegisterUserR = RegisterUserR
+    { rurUserId :: Int
+    , rurName   :: String
+    } deriving (Show, Eq)
+
+instance FromJSON RegisterUserR where
+    parseJSON (Object v) =
+        RegisterUserR <$> (v .: "userId") <*> (v .: "name")
